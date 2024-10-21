4eb021689e579b8baa4660f41add7b6957658930
ajmiller611 <ajmiller611@live.com>
2024-10-21 17:15:54 -0400

    added cart totaling functionality

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,14 +1,16 @@
 import React, { useState,useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
-import addItem from './CartSlice';
+import { addItem } from './CartSlice';
+import { useDispatch } from 'react-redux';
 function ProductList() {
+    const dispatch = useDispatch();
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
 
     const handleAddToCart = (product) => {
-        dispatchEvent(addItem(product));
+        dispatch(addItem(product));
         setAddedToCart((prevState) => ({
             ...prevState,
             [product.name]: true,
@@ -280,7 +282,7 @@ const handlePlantsClick = (e) => {
         <div className="product-grid">
             {plantsArray.map((category, index) => (
                 <div key={index}>
-                    <h1><div className='plant_heading'>{category.category}</div></h1>
+                    <h1 className='plantname_heading'><div className='plant_heading'>{category.category}</div></h1>
                     <div className='product-list'>
                         {category.plants.map((plant, plantIndex) => (
                             <div className='product-card' key={plantIndex}>
